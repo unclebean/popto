cfb5de14b2458b8fd15b08ea68bd46423904ddde
unclebean <unclebean2013@gmail.com>
2017-03-15 01:02:16 +0800

    remove compiled files

--- resources/public/js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [popto-ui.app] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "app", :websocket-url "ws://localhost:5309/figwheel-ws"})
-
